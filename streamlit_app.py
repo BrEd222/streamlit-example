a49b64f67ec06a6fc720d806f3c740c3f971ac2a
BrEd222 <113176101+BrEd222@users.noreply.github.com>
2024-01-15 20:12:04 +0100

    sss

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,21 +6,38 @@ import requests
 from pandas import json_normalize
 
 
+@st.cache_data
+def get_ninja(url):
+    data_json = requests.get(url)
+    pd_json = json_normalize(data_json,record_path='lines')    
+    return data
+
 
 st.title('Test Grafico POE')
-url = "https://poe.ninja/api/data/itemhistory?league=Affliction&type=UniqueFlask&itemId=20932"
-data_json = requests.get(url)
-
-poedf=pd.DataFrame(data_json.json())
-#print(poedf['value'])
-st.bar_chart(poedf['value'])
 
 url = "https://poe.ninja/api/data/currencyoverview?league=Affliction&type=Currency"
 r = requests.get(url)
-data_json = r.json()
+curr_list = get_ninja(url)
 #print(data_json)
 
-pd_json = json_normalize(data_json,record_path='lines')
+curr_list = json_normalize(curr_list,record_path='lines')
 
-selection = st.selectbox('Select', pd_json['currencyTypeName'].values)
-st.text([selection])
+selection = st.selectbox('Select', curr_list['currencyTypeName'].values)
+#st.text([selection])
+
+id=pd_json[pd_json['currencyTypeName']==selection]['pay.pay_currency_id'].values[0]
+ids=str(int(id))
+
+url_history = 'https://poe.ninja/api/data/currencyhistory?league=Affliction&type=Currency&currencyId='+ids
+
+curr_hist = get_ninja(url_history)
+
+#url = "https://poe.ninja/api/data/itemhistory?league=Affliction&type=UniqueFlask&itemId=20932"
+
+#Currency overview
+#https://poe.ninja/api/data/currencyoverview?league=Affliction&type=Currency
+
+#Currency history
+#https://poe.ninja/api/data/currencyhistory?league=Affliction&type=Currency&currencyId=22
+
+st.bar_chart(curr_hist['value'])
