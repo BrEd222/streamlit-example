fd0b5465c7572950a6b26f93a360deb003734990
BrEd222 <113176101+BrEd222@users.noreply.github.com>
2024-01-15 21:25:16 +0100

    ss

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,8 @@ url_history = 'https://poe.ninja/api/data/currencyhistory?league=Affliction&type
 curr_hist = ninja_cur_hist(url_history)
 #st.bar_chart(curr_hist['value'])
 st.line_chart(curr_hist['value'])#,x="Chaos Equivalent",y="Giorno di Lega")
-st.plotly_chart(curr_hist['value'], use_container_width=True)
+fig = px.line(curr_hist['value'], x="Giorno di lega", y="Chaos Equivalent", title='Andamento')
+st.plotly_chart(fig, use_container_width=True)
 
 #url = "https://poe.ninja/api/data/itemhistory?league=Affliction&type=UniqueFlask&itemId=20932"
 
