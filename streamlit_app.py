555c6de25fdcb606698f672004f0d0559c089fc0
BrEd222 <113176101+BrEd222@users.noreply.github.com>
2024-01-15 19:36:25 +0100

    mod

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,23 @@ import pandas as pd
 import numpy as np
 import urllib.request, json 
 import requests
+from pandas import json_normalize
+
 
 
 st.title('Test Grafico POE')
 url = "https://poe.ninja/api/data/itemhistory?league=Affliction&type=UniqueFlask&itemId=20932"
 data_json = requests.get(url)
-#print(data_json.json())
 
 poedf=pd.DataFrame(data_json.json())
 #print(poedf['value'])
 st.bar_chart(poedf['value'])
 
+url = "https://poe.ninja/api/data/currencyoverview?league=Affliction&type=Currency"
+r = requests.get(url)
+data_json = r.json()
+#print(data_json)
 
+pd_json = json_normalize(data_json,record_path='lines')
 
-
+st.selectbox('Select', [1,2,3])
